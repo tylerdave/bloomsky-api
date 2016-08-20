869ee6873e966614b77788be73b34bfbdbb33604
Dave Forgac <tylerdave@tylerdave.com>
2016-08-20 02:03:48 -0400

    reformat data received from API

--- bloomsky_api/bloomsky_api.py
+++ bloomsky_api/bloomsky_api.py
@@ -1,7 +1,85 @@
 import os
 import requests
+from datetime import datetime
 
 DEFAULT_API_URL = 'https://thirdpartyapi.appspot.com/api/skydata/'
+
+
+class BloomSkyAPIResponse(object):
+    field_mapping = [
+            ('ALT', 'altitude'),
+            ('CityName', 'city_name'),
+            ('DST', 'is_dst'),
+            ('DeviceID', 'device_id'),
+            ('DeviceName', 'device_name'),
+            ('FullAddress', 'full_address'),
+            ('LAT', 'latitude'),
+            ('LON', 'longitude'),
+            ('NumOfFavorites', 'favorites_count'),
+            ('NumOfFollowers', 'followers_count'),
+            ('RegisterTime', 'registered_timestamp'),
+            ('Searchable', 'is_searchable'),
+            ('StreetName', 'street_name'),
+            ('UTC', 'utc_offset'),
+            ('VideoList', 'video_urls'),
+            ]
+    data_field_mapping = [
+            ('Humidity', 'humidity'),
+            ('ImageTS', 'image_timestamp'),
+            ('ImageURL', 'image_url'),
+            ('Luminance', 'luminance'),
+            ('Night', 'is_night'),
+            ('Pressure', 'pressure'),
+            ('Rain', 'is_raining'),
+            ('TS', 'data_timestamp'),
+            ('Temperature', 'temperature'),
+            ('UVIndex', 'uv_index'),
+            ('Voltage', 'voltage'),
+            ]
+
+    def __init__(self, response):
+        self._raw_response = response
+        self._json = response.json()
+        self.data = [self._remap_data(device) for device in self._json]
+        self._normalize_data()
+
+    @classmethod
+    def _remap_data(cls, response_data):
+        remapped_data = {}
+        for old_name, new_name in cls.field_mapping:
+            remapped_data[new_name] = response_data.get(old_name)
+        remapped_data['outdoor'] = {}
+        for old_name, new_name in cls.data_field_mapping:
+            remapped_data['outdoor'][new_name] = \
+                    response_data['Data'].get(old_name)
+        indoor = response_data.get('Point', {})
+        remapped_data['indoor'] = {
+                'humidity': indoor.get('Humidity'),
+                'temperature': indoor.get('Temperature'),
+                }
+        return remapped_data
+
+    def _normalize_data(self):
+        for device in self.data:
+            device['is_dst'] = bool(device['is_dst'])
+            device['outdoor']['data_timestamp'] = self._timestamp_to_iso_format(
+                    device['outdoor']['data_timestamp'])
+            device['outdoor']['image_timestamp'] = self._timestamp_to_iso_format(
+                    device['outdoor']['image_timestamp'])
+            device['outdoor']['uv_index'] = int(device['outdoor']['uv_index'])
+            device['registered_timestamp'] = self._timestamp_to_iso_format(
+                    device['registered_timestamp'])
+
+    @staticmethod
+    def _timestamp_to_iso_format(timestamp):
+        try:
+            return datetime.fromtimestamp(timestamp).isoformat()
+        except:
+            return None
+
+    def __repr__(self):
+        return "{0}".format(self._json)
+
 
 class BloomSkyClient(object):
     """ A client for interacting with the BloomSky API """
@@ -10,11 +88,15 @@ class BloomSkyClient(object):
         self.api_key = self._get_api_key(api_key)
         self.api_url = self._get_api_url(api_url)
 
-    def get_data(self):
+    def request_data(self):
         headers = {'Authorization': self.api_key}
         response = requests.get(self.api_url, headers=headers)
         response.raise_for_status()
-        return response.json()
+        return BloomSkyAPIResponse(response)
+
+    def get_data(self):
+        response = self.request_data()
+        return response.data
 
     @staticmethod
     def _get_api_key(provided_api_key=None):
@@ -29,4 +111,6 @@ class BloomSkyClient(object):
             return provided_api_url
         else:
             return DEFAULT_API_URL
+
+
 
