b7b7a7b9615890bd594f767d407d150da65974b9
Dave Forgac <tylerdave@tylerdave.com>
2016-08-22 00:51:08 -0400

    add test for cli

---
+++ tests/test_cli.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_bloomsky_api
+----------------------------------
+
+Tests for `bloomsky_api` module.
+"""
+
+import json
+import responses
+import unittest
+from click.testing import CliRunner
+
+import bloomsky_api.cli
+from .testing_data import GOOD_API_RESPONSE, GOOD_CLI_OUTPUT_JSON
+
+class TestBloomskyCLI(unittest.TestCase):
+
+    def setUp(self):
+        self.runner = CliRunner()
+
+    @responses.activate
+    def test_cli(self):
+        url = bloomsky_api.bloomsky_api.DEFAULT_API_URL
+        responses.add(responses.GET, url,
+                  body=GOOD_API_RESPONSE, status=200,
+                  content_type='application/json')
+        result = self.runner.invoke(bloomsky_api.cli.cli, ['--json-indent', '1'])
+        output = result.output
+        assert(json.loads(output) == json.loads(str(GOOD_CLI_OUTPUT_JSON)))
+
--- tests/testing_data.py
+++ tests/testing_data.py
@@ -2,3 +2,48 @@ GOOD_API_RESPONSE = '[{"DeviceID":"93E18E","LAT":41.482582100000002,"LON":-81.80
 GOOD_DATA_RESPONSE_JSON = """[{"street_name": "Lakeland Avenue", "utc_offset": -4, "outdoor": {"uv_index": 1, "luminance": 3460, "temperature": 71.42, "is_raining": false, "humidity": 90, "pressure": 29.1757388, "is_night": false, "image_url": "http://storage.googleapis.com/bloomsky-img/eaB1rJytnpS5mZ23qJ1krZaunZSomJY=.jpg", "image_timestamp": "2016-08-21T08:43:21-04:00", "data_timestamp": "2016-08-21T08:43:21-04:00", "voltage": 2593}, "favorites_count": 17, "altitude": 213.5209045410156,
 "indoor": {"temperature": 72.10400000000001, "humidity": 37}, "followers_count": 53, "full_address": "Lakeland Ave, Lakewood, OH, US", "device_name": "Lakeland Ave", "latitude": 41.4825821, "is_dst": true, "video_urls": ["http://storage.googleapis.com/bloomsky-video/eaB1rJytnpS5mZ23_-4_2016-08-16.mp4", "http://storage.googleapis.com/bloomsky-video/eaB1rJytnpS5mZ23_-4_2016-08-17.mp4", "http://storage.googleapis.com/bloomsky-video/eaB1rJytnpS5mZ23_-4_2016-08-18.mp4",
 "http://storage.googleapis.com/bloomsky-video/eaB1rJytnpS5mZ23_-4_2016-08-19.mp4", "http://storage.googleapis.com/bloomsky-video/eaB1rJytnpS5mZ23_-4_2016-08-20.mp4"], "longitude": -81.8059589, "is_searchable": true, "city_name": "Lakewood", "registered_timestamp": "2015-09-05T17:09:41-04:00", "device_id": "93E18E"}]"""
+GOOD_CLI_OUTPUT_JSON = """
+[
+ {
+  "street_name": "Lakeland Avenue",
+  "utc_offset": -4,
+  "outdoor": {
+   "uv_index": 1,
+   "luminance": 3460,
+   "temperature": 71.42,
+   "is_raining": false,
+   "humidity": 90,
+   "pressure": 29.1757388,
+   "is_night": false,
+   "image_url": "http://storage.googleapis.com/bloomsky-img/eaB1rJytnpS5mZ23qJ1krZaunZSomJY=.jpg",
+   "image_timestamp": "2016-08-21T08:43:21-04:00",
+   "data_timestamp": "2016-08-21T08:43:21-04:00",
+   "voltage": 2593
+  },
+  "favorites_count": 17,
+  "altitude": 213.5209045410156,
+  "indoor": {
+   "temperature": 72.10400000000001,
+   "humidity": 37
+  },
+  "followers_count": 53,
+  "full_address": "Lakeland Ave, Lakewood, OH, US",
+  "device_name": "Lakeland Ave",
+  "latitude": 41.4825821,
+  "is_dst": true,
+  "video_urls": [
+   "http://storage.googleapis.com/bloomsky-video/eaB1rJytnpS5mZ23_-4_2016-08-16.mp4",
+   "http://storage.googleapis.com/bloomsky-video/eaB1rJytnpS5mZ23_-4_2016-08-17.mp4",
+   "http://storage.googleapis.com/bloomsky-video/eaB1rJytnpS5mZ23_-4_2016-08-18.mp4",
+   "http://storage.googleapis.com/bloomsky-video/eaB1rJytnpS5mZ23_-4_2016-08-19.mp4",
+   "http://storage.googleapis.com/bloomsky-video/eaB1rJytnpS5mZ23_-4_2016-08-20.mp4"
+  ],
+  "longitude": -81.8059589,
+  "is_searchable": true,
+  "city_name": "Lakewood",
+  "registered_timestamp": "2015-09-05T17:09:41-04:00",
+  "device_id": "93E18E"
+ }
+]
+"""
+
