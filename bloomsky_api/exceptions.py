d45be6bd00aac252decc950c7c5bfe9560187430
Daniel Shokouhi <dshokouhi@gmail.com>
2019-10-07 09:07:19 -0700

    Additional exceptions

--- bloomsky_api/exceptions.py
+++ bloomsky_api/exceptions.py
@@ -3,3 +3,12 @@ class BloomSkyAPIError(Exception):
 
 class APIKeyMissing(BloomSkyAPIError):
     pass
+
+class APIKeyInvalid(BloomSkyAPIError):
+    pass
+
+class BloomSkyConnection(BloomSkyAPIError):
+    pass
+
+class NoDevicesFound(BloomSkyAPIError):
+    pass
